d02685b597fadab6d71f2388be3dcab97815d23b
Polo Dev <polodev10@gmail.com>
2017-11-26 07:37:40 +0600

    added todo

---
+++ src/data.js
@@ -0,0 +1,19 @@
+const todos = [
+  {
+    id: 1,
+    text: 'clean the house',
+    completion: false
+  },
+  {
+    id: 2,
+    text: 'go to market',
+    completion: true
+  },
+  {
+    id: 3,
+    text: 'purchase some food',
+    completion: false
+  }
+];
+
+export default todos;
